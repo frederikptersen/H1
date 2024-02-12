e024063fac446746c7c6f8d37d6ed8dc21067586
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 13:38:10 +0100

    Made the assignment "Sort characters descending".

---
+++ Strings/SortCharactersDescending/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SortChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SortChars("hello"));
+            Console.WriteLine(SortChars("hello world"));
+        }
+
+        public static char[] SortChars(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Sort(charArray);
+            Array.Reverse(charArray);
+            return charArray;
+        }
+    }
+}
+
