f14714b6a09973cc65338c7f49a82993cdeb53d6
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 12:57:54 +0100

    Made the "String in reverse order"

---
+++ Strings/StringInReverseOrder/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringInReverse("Hello"));
+            Console.WriteLine(StringInReverse("Goodbye you!"));
+        }
+
+        public static string StringInReverse(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
