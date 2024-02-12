aabd58670762f5b687ddbad0bf3c760377c76539
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 13:32:09 +0100

    Made the "How many occurences".

---
+++ Strings/HowManyOccurences/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HowManyOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManyOccurences("do it now", "do"));
+            Console.WriteLine(HowManyOccurences("do it now", "o"));
+            Console.WriteLine(HowManyOccurences("do it now", "it"));
+        }
+
+        public static int HowManyOccurences(string str, string subStr)
+        {
+            int count = 0;
+            int i = 0;
+            while ((i = str.IndexOf(subStr, i)) != -1)
+            {
+                i += subStr.Length;
+                count++;
+            }
+            return count;
+        }
+    }
+}
+    
+
