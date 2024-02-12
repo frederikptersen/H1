45dc7a76b7c0edc71fc77e0a73ee7672cbc61759
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 12:49:33 +0100

    Made the "Length of string"

---
+++ Strings/LengthOfString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LengthOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfString("computerprogram"));
+            Console.WriteLine(LengthOfString("ice cream"));
+        }
+
+        public static int LengthOfString(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
