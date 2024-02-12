66ebb9fbca31a382fb682198da5d768aaf8dc605
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 12:38:34 +0100

    Made the "IsPalindrome" assignment

---
+++ Strings/IsPalindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;  
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("clock"));
+            Console.WriteLine(IsPalindrome("deed"));
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            int i = 0;
+            int j = str.Length - 1;
+            while (i < j)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
+
+
+
+
+
+
