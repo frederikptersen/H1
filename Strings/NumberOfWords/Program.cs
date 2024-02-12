1e15f54bd1625c8046ea3581f60d344bcc422dd4
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 13:06:59 +0100

    Made the assignment "NumberOfWords"

---
+++ Strings/NumberOfWords/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberOfWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine(CountWords("Hello World! how are you?"));
+            Console.WriteLine(CountWords("OK"));
+        }
+
+        public static int CountWords(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int count = 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
+
