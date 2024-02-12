60550b3e458a4dd9ff1b0a3b11ed155b638532c5
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 13:43:05 +0100

    Made the "Compress string"

---
+++ Strings/CompressString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CompressString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompressString("aaabbbcc"));
+            Console.WriteLine(CompressString("aaabbbccdd"));
+        }
+
+        public static string CompressString(string input)
+        {
+            string result = "";
+            int count = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i + 1 < input.Length && input[i] == input[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result += input[i] + count.ToString();
+                    count = 1;
+                }
+            }
+            return result;
+        }
+    }
+}
