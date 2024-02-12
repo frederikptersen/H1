f636ec8d4dfaa712d10ed2b4dca4dc3443e88fce
frederikptersen <126653885+frederikptersen@users.noreply.github.com>
2024-02-12 12:11:39 +0100

    Made "Add seperator"

---
+++ Strings/AddSeperator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AddSeparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddSeparator("ABCD", "^"));
+            Console.WriteLine(AddSeparator("chocolate", "-"));
+        }
+        public static string AddSeparator(string str, string separator)
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                result += str[i];
+                if (i != str.Length - 1)
+                {
+                    result += separator;
+                }
+            }
+            return result;
+        }
+    }
+}
